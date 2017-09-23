a5e40cfa157f3f42d67ab283dab8dc9b0c1bb4c4
hotbug22 <hotbug22@gmail.com>
2017-09-23 15:24:05 +0900

    git

---
+++ httpexam/request.response.js
@@ -0,0 +1,34 @@
+var http = require('http');
+var fs = require('fs');
+
+http.createServer(function(req, res){
+    //
+    if (req.method == 'GET') {
+        console.log(req.url+'get');
+        fs.readFile('index.html', function(err,data){
+
+            if(!err){
+            	res.writeHead(200,{'Content-Type':'text/html'});
+            	res.end(data);
+            } else{
+            	res.writeHead(404);
+            	res.end();
+            }
+
+        });
+
+    }else if (req.method == 'POST') {
+
+        console.log(req.url+'POST');
+        req.on('data', function(data){
+        	res.writeHead(200,{'Content-Type' : 'text/html'});
+        	res.end(data);
+        });
+
+    }
+}).listen(52273, function(){
+
+	console.log('Server running');
+
+
+});
