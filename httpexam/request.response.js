e65fb814669664e005fde711938f72493376a64e
hotbug22 <hotbug22@gmail.com>
2017-09-23 16:38:49 +0900

    dl

--- httpexam/request.response.js
+++ httpexam/request.response.js
@@ -1,11 +1,15 @@
 var http = require('http');
 var fs = require('fs');
+var ejs = require('ejs');
+var jade = require('jade');
 
 http.createServer(function(req, res){
     //
     if (req.method == 'GET') {
         console.log(req.url+'get');
-        fs.readFile('index.html', function(err,data){
+        
+        if(req.url == '/'){
+        	fs.readFile('index.html', function(err,data){
 
             if(!err){
             	res.writeHead(200,{'Content-Type':'text/html'});
@@ -15,7 +19,33 @@ http.createServer(function(req, res){
             	res.end();
             }
 
-        });
+         });
+
+        }else if (req.url == '/ejs'){
+
+             fs.readFile('templete.ejs' , 'utf8' , function(err,data){
+             	if(!err){
+             		var html = ejs.render(data,{name:'hdkjls' , description:'hello world form EJS'});
+             		res.writeHead(200, {'Content-Type' : "text/html"});
+             		res.end(html);
+             	}
+             });
+
+
+        } else if (req.url =='/jade'){
+            fs.readFile ('templete.jade' , 'utf8',function(error,data){
+            	var fn = jade.compile(data);
+
+            	res.writeHead(200 , {'Content-Type' : 'text/html'});
+            	res.end(fn({
+            		name: 'rintiantta', descripion: 'Hello jade with node js'
+            	}));
+            });
+
+
+
+
+        }
 
     }else if (req.method == 'POST') {
 
@@ -29,6 +59,5 @@ http.createServer(function(req, res){
 }).listen(52273, function(){
 
 	console.log('Server running');
-
 
 });
