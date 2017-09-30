706663e35e218f13e6d3d3221d0e14bb4d2d9a04
hotbug22 <hotbug22@gmail.com>
2017-09-30 16:11:26 +0900

    userActivity

--- restfulExam/rest.js
+++ restfulExam/rest.js
@@ -1,129 +1,165 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var app = express();
+app.use(bodyParser.urlencoded({extended:false}));
 
 var mysql = require('mysql');
-var mongodb = require('mongodb');
-
 var connection = mysql.createConnection({
   host     : 'localhost',
   user     : 'root',
   password : 'test1234',
   database : 'restful'
-});
- 
+}); 
 connection.connect();
- 
-connection.query('SELECT 1 + 1 AS solution', function (error, results, fields) {
-  if (error) throw error;
-  console.log('The solution is: ', results[0].solution);
+///////////////////////////////////
+var MongoClient = require('mongodb').MongoClient;
+var url = 'mongodb://localhost:27017/restful';
+var dbObj = null;
+MongoClient.connect(url, function(err, db) {
+  console.log("Connected correctly to server");
+  dbObj = db;
 });
- 
-
-var app = express();
-app.use(bodyParser.urlencoded({extended : false}));
-
-var users = [];
-
-app.get('/user' , function (req,res) {
-	//res.send(JSON.stringify(users));
-	// body...
-    connection.query('select * from user' , function(err,results,fields){
-        if(err){
-        	res.send(JSON.stringify(err));
-        }else{
-        	res.send(JSON.stringify(results));
-        }
-    });
-
+app.get('/user/message',function(req,res) {
+	console.log(req.query.sender_id);
+	var condition = {};
+	if (req.query.sender_id != undefined)
+		condition = {sender_id:req.query.sender_id};
+	var messages = dbObj.collection('messages');
+	messages.find(condition)
+		.toArray(function(err, results){
+		if (err) {
+			res.send(JSON.stringify(err));
+		} else {
+			res.send(JSON.stringify(results));
+		}
+	});
+});
+var ObjectID = require('mongodb').ObjectID;
+app.get('/user/message/:id',function(req,res) {
+	var messages = dbObj.collection('messages');
+	messages.findOne(
+		{_id:ObjectID.createFromHexString(req.params.id)},
+		function(err, result){
+			if (err) {
+				res.send(JSON.stringify(err));
+			} else {
+				res.send(JSON.stringify(result));
+			}
+		});
+});
+app.post('/user/message',function(req,res) {
+	console.log(req.body.sender_id);
+	console.log(req.body.reciever_id);
+	console.log(req.body.message);
+	connection.query(
+		'select id,name from user where id=? or id=?',
+		[req.body.sender_id,req.body.reciever_id],
+		function(err, results, fields) {
+			if (err) {
+				res.send(JSON.stringify(err));
+			} else {
+				var sender = {};
+				var reciever = {};
+				for (var i = 0; i < results.length; i++){
+					if (results[i].id == 
+						Number(req.body.sender_id)) {
+						sender = results[i];
+					}
+					if (results[i].id ==
+						Number(req.body.reciever_id)) {
+						reciever = results[i];
+					}
+				}
+				var object = {
+					sender_id:req.body.sender_id,
+					reciever_id:req.body.reciever_id,
+					sender:sender, reciever:reciever,
+					message:req.body.message,
+					created_at:new Date()
+				}
+				var messages = dbObj.collection('messages');
+				messages.save(object, function(err, result){
+					if (err) {
+						res.send(JSON.stringify(err));
+					} else {
+						res.send(JSON.stringify(result));
+					}
+				});
+			}
+		});
+});
+app.delete('/user/message/:id',function(req,res) {
+	var messages = dbObj.collection('messages');
+	messages.remove(
+		{_id:ObjectID.createFromHexString(req.params.id)},
+		function(err, result){
+			if (err) {
+				res.send(JSON.stringify(err));
+			} else {
+				res.send(JSON.stringify(result));
+			}
+		});
 });
 
-app.get('/user/:id' , function (req,res) {
+app.get('/user',function(req,res) {
+	connection.query('select * from user', 
+		function(err,results,fields) {
+			if (err) {
+				res.send(JSON.stringify(err));
+			} else {
+				res.send(JSON.stringify(results));
+			}
+		});
+});
+app.get('/user/:id',function(req,res){
 	connection.query('select * from user where id=?',
-		[req.params.id], function(err, results,fields){
-             if(err){
-        	res.send(JSON.stringify(err));
-            }else{
-
-            	if(results.length > 0){
-                    res.send(JSON.stringify(results[0]));
-            	}else{
-                    res.send(JSON.stringify({}));
-            	}
-            }
-		});
-	/*var select_index = -1;
-	for (var i = 0; i < users.length ; i++) {
-        var obj = users[i];
-        if(obj.id == Number(req.params.id)){
-        	select_index = i;
-        	break;
-        }
-
-
-	}
-	if(select_index == -1){
-        res.send(JSON.stringify({}));
-
-
-	}else
-	{
-
-		res.send(JSON.stringify(users[select_index]));
-	}
-	*/
-
-
-
-	
-});
-
-app.post('/user' , function (req,res) {
-    connection.query('insert into user(name,age) values(?,?)',
-    	[req.body.name , req.body.age], 
-    	function(err,result){
-    		if(err){
-    			res.send(JSON.stringify(err));
-    		}else{
-    			res.send(JSON.stringify(result));
-    		}
-    	});
-
-    /*var name = req.body.name;
-    var age = Number(req.body.age);
-    var obj = {id:users.length+1 , name:name, age:age};
-
-    users.push(obj);
-
-	res.send(JSON.stringify({api :'add user info',
-                              result : true }));
-	// body...*/
-});
-
-app.put('/user/:id' , function (req,res) {
-	connection.query('update user set name=?, age=? where id =?',
-		[req.body.name , req.body.age , req.params.id],
-		function(err ,result){
-			if (err){
+		[req.params.id], function(err, results, fields) {
+			if (err) {
 				res.send(JSON.stringify(err));
-			}else {
+			} else {
+				if (results.length > 0) {
+					res.send(JSON.stringify(results[0]));
+				} else {
+					res.send(JSON.stringify({}));
+				}
+				
+			}
+		});
+});
+app.post('/user',function(req,res){
+	connection.query(
+		'insert into user(name,age) values(?,?)',
+		[ req.body.name, req.body.age ], 
+		function(err, result) {
+			if (err) {
+				res.send(JSON.stringify(err));
+			} else {
+				res.send(JSON.stringify(result));
+			}
+		})
+});
+app.put('/user/:id',function(req,res){
+	connection.query(
+		'update user set name=?,age=? where id=?',
+		[ req.body.name, req.body.age, req.params.id ],
+		function(err, result) {
+			if (err) {
+				res.send(JSON.stringify(err));
+			} else {
+				res.send(JSON.stringify(result));
+			}
+		})
+});
+app.delete('/user/:id',function(req,res){
+	connection.query('delete from user where id=?',
+		[ req.params.id ], function(err, result) {
+			if (err) {
+				res.send(JSON.stringify(err));
+			} else {
 				res.send(JSON.stringify(result));
 			}
 		});
 });
-
-app.delete('/user/:id' , function (req,res) {
-   connection.query('delete from user where id = ?',
-   	[req.params.id], 
-   	function(err, result){
-   		if (err){
-				res.send(JSON.stringify(err));
-			}else {
-				res.send(JSON.stringify(result));
-			}
-		});
-});
-
-app.listen(52273,function () {
-	// body...
-	console.log('server is running');
+app.listen(52273,function() {
+	console.log('Server running');
 });
